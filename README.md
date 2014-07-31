fbaea4121f67df7339afbd219dcd2ab6c0f509c2
gitmarmina <administrator@Administrators-iMac.local>
2014-07-31 00:13:54 -0400

    inital uplaod

---
+++ README.md
@@ -0,0 +1,21 @@
+py_mailer
+=========
+
+send emails from python without any SMTP server.
+
+Usage:
+
+import py_mailer 
+
+
+Sender='youremail@example.com'
+
+Recipient='recipient@example.com'
+
+subject='hi subj' 
+
+message='Hi body'
+
+
+
+py_mailer.sendEmail(Sender, Recipient, subject, message)
---
+++ py_mailer.py
@@ -0,0 +1,51 @@
+import dns.resolver
+import smtplib
+from email.mime.text import MIMEText
+
+
+
+def printMXserver(domain):
+	answers = dns.resolver.query(domain, 'MX')
+	for rdata in answers:
+		print 'Host', rdata.exchange, 'has preference', rdata.preference
+
+def getMXserver(domain):
+	try:
+		answers = dns.resolver.query(domain, 'MX')
+		for rdata in answers:
+			return str(rdata.exchange).rstrip('.')
+	except:
+		return None
+		
+
+def sendEmail(Sender, Recipient, subject, message, Server=None):
+	msg = MIMEText(message)
+	msg['Subject'] = subject
+	msg['From'] = Sender
+	msg['To'] = Recipient
+
+	if Server is None:
+		Server = getMXserver(Recipient.split("@")[1])
+	if Server is None:
+		print 'No MX record found to relay message'
+		return False
+	
+	print "Server:", Server
+	raw_input(">>")
+
+	s = smtplib.SMTP(Server)
+	s.sendmail(Sender, [Recipient], msg.as_string())
+	s.quit()
+	
+	
+
+
+"""
+  USAGE
+"""
+
+Sender='youremail@example.com' 
+Recipient='recipient@example.com' 
+subject='hi subj' 
+message='Hi body'
+sendEmail(Sender, Recipient, subject, message)
